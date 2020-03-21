723cb06f959800b55c6406ad4f61c93c73717835
Lee-kangchan <lsc4237@gmail.com>
2020-03-21 22:57:13 +0900

    별 찍기-2

---
+++ Basic/src/q2439/Main.java
@@ -0,0 +1,28 @@
+package q2439;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+public class Main{
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(bf.readLine().trim());
+		
+		for(int i = 1 ; i <=T ; i++) {
+			for(int k = T; k > i ; k--) {
+				bw.write(" ");
+			}
+			
+            for(int j = 1 ; j <=i ; j++) {
+            	bw.write("*");
+            }
+            bw.write("\n");
+		}
+        bw.flush();
+        bw.close();
+}
+}
