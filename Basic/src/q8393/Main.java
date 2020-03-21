911127c66a586b42acc4392e992f1b95494c5cda
Lee-kangchan <lsc4237@gmail.com>
2020-03-21 22:57:57 +0900

    합

---
+++ Basic/src/q8393/Main.java
@@ -0,0 +1,19 @@
+package q8393;
+
+import java.util.*;
+public class Main{
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int T;
+		T= sc.nextInt();
+		
+		int sum =0 ;
+		for(int i = 0 ; i <=T; i++) {
+			sum = sum + i;
+		}
+		System.out.println(sum);
+	}
+}
