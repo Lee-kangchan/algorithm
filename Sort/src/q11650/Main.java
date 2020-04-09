8b03c878c6e89c240c9efac250bc96caf2fa688b
IntelliJSQLConnection <lsc4237@gamil.com>
2020-04-09 21:20:34 +0900

    좌표 정렬하기(합병 실패)

--- Sort/src/q11650/Main.java
+++ Sort/src/q11650/Main.java
@@ -14,12 +14,14 @@ public class Main {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int T = sc.nextInt();
+		int T = Integer.parseInt(bf.readLine().trim());
+		String S[] = new String[2] ;
 		int A[][] = new int[T][2];
 		
 		for(int i = 0 ; i <T ; i ++) {
-			A[i][0]= sc.nextInt();
-			A[i][1] = sc.nextInt();
+			S = bf.readLine().trim().split(" ");
+			A[i][0]= Integer.parseInt(S[0]);
+			A[i][1] = Integer.parseInt(S[1]);
 		}
 		
 		int M;
@@ -37,15 +39,46 @@ public class Main {
 			A[i][1] = A[am][1];
 			A[am][0] = w;
 			A[am][1] = W;
-			System.out.println(A[i][0]+" "+A[i][1]);
+			bw.write(A[i][0]+" "+A[i][1]+"\n");
 		}
-		
-
-		
-		
-	
-	
+	bw.close();
+	bf.close();
 	}
 
+	
+//
+//	public void marge(int A[],int p,int r) {
+//		if(p<r) {
+//			int E = (p+r)/2;
+//			marge(A,p,E);
+//			marge(A,E+1,r);
+//			margeSort(A,p,E,r);
+//			
+//		}
+//    }
+//	public void margeSort(int A[], int p ,int  q,int  r)
+//	{
+//		int i= p , j = q+1 , k = p;
+//		int tmp[] = new int[A.length];
+//		
+//		while(i<=q && j<=r) {
+//			if(A[i]<=A[j]) {
+//				tmp[k++] = A[i++];
+//			}
+//			else {
+//				tmp[k++] = A[j++];
+//			}
+//		}
+//		while( i <=q ) {
+//			tmp[k++] = A[i++];
+//		}
+//		while(j<=r) {
+//			tmp[k++] = A[j++];
+//		}
+//		for(i = p ; i<=r ; i++) {
+//			A[i] = tmp[i];
+//		}
+//	}
+//	
 }
 
